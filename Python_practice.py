83b76229a3e7e3ce19fbc64319021995ddd1f4be
Joyce Wang <joycewang@Joyces-MacBook-Pro.local>
2020-05-31 22:53:58 -0700

    submitting my answer with a message.

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+print("Hello World")
+counties=["Arapahoe","Denver","Jefferson"]
+if counties[1]=='Denver':
+    print(counties[1])
+if counties[3]!='Jefferson'
+    print(counties[2])
+temperature=int(input("What is the temperture outside"))
+if temperature>80:
+    print("Turn on the AC.")
+else:
+    print("Open teh windows.")     
+counties=["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+voting_data[{'county': 'Arapahoe', 'registered_voters': 422829}, {'county': 'Denver', 'registered_voters': 463353}, {'county': 'Jefferson', 'registered_voters': 432438}]
+voting_data
